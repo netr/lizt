3e09a6301c9a75d95834e6edc3b932973f8f1904
NETR <programmatical@gmail.com>
2022-12-22 04:34:12 -0800

    create NextSeed() for tracking when seeds are being planted

--- seeder.go
+++ seeder.go
@@ -47,27 +47,44 @@ func (si *SeedingIterator) inc() {
 
 // Next returns the next line from the iterator and will automatically seed every PlantEvery() lines.
 func (si *SeedingIterator) Next(count int) ([]string, error) {
+	lines, _, err := si.nextSeed(count)
+	if err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+// NextSeed returns the next line from the iterator and will automatically seed every PlantEvery() lines.
+// The difference from the interface Next() is that this returns a bool indicating if a seed was planted.
+func (si *SeedingIterator) NextSeed(count int) ([]string, bool, error) {
+	return si.nextSeed(count)
+}
+
+func (si *SeedingIterator) nextSeed(count int) ([]string, bool, error) {
 	var lines []string
+	seeded := false
 	for i := 0; i < count; i++ {
 		sent := si.Pointer() + uint64(si.Planted())
 		if sent%uint64(si.PlantEvery()) == 0 {
 			seed, err := si.seedIter.Next(1)
 			if err != nil {
-				return nil, fmt.Errorf("seed iter next: %w", err)
+				return nil, seeded, fmt.Errorf("seed iter next: %w", err)
 			}
+			seeded = true
 			si.inc()
 			lines = append(lines, seed[0])
 		} else {
 			next, err := si.PointerIterator.Next(1)
 			if err != nil {
 				if len(lines) == 0 {
-					return nil, fmt.Errorf("file: %s -> %w", si.Name(), err)
+					return nil, seeded, fmt.Errorf("file: %s -> %w", si.Name(), err)
 				}
-				return lines, nil
+				return lines, seeded, nil
 			}
 			lines = append(lines, next[0])
 		}
 	}
 
-	return lines, nil
+	return lines, seeded, nil
 }
--- seeder_test.go
+++ seeder_test.go
@@ -77,3 +77,42 @@ func TestSeeder_Next_RoundRobin(t *testing.T) {
 		}
 	}
 }
+
+func TestSeeder_NextSeed(t *testing.T) {
+	numbers := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	iter := lizt.NewSliceIterator(nameNumbers, numbers, false)
+
+	seeds := []string{"seeder1", "seeder2", "seeder3", "seeder4", "seeder5", "seeder6", "seeder7", "seeder8", "seeder9", "seeder10"}
+	seedIter := lizt.NewSliceIterator("seedIter", seeds, true)
+
+	seed := lizt.NewSeedingIterator(
+		lizt.SeedingIteratorConfig{
+			PointerIter: iter,
+			SeedIter:    seedIter,
+			PlantEvery:  2,
+		},
+	)
+	next, seeded, err := seed.NextSeed(6)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if !seeded {
+		t.Errorf("Expected seeded to be true")
+	}
+
+	expected := map[int]string{
+		0: "seeder1",
+		1: "1",
+		2: "seeder2",
+		3: "2",
+		4: "seeder3",
+		5: "3",
+	}
+
+	for k, v := range expected {
+		if next[k] != v {
+			t.Errorf("Expected %s, got %s", v, next[k])
+		}
+	}
+}
